d7326c433ecfe892a1cedd7c96639e5de490df4c
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-06 19:45:06 -0800

    Render shipping information element

---
+++ client/components/ShippingInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ShippingInfo = (props) => {
+
+  return (
+    <div>
+      Free standard shipping and 30-day free returns, only with NikePlus. 
+      <a href=''>Learn more. </a>
+      <a href=''>Return policy exclusions apply. </a>
+      <br />
+      <li>Standard / Arrives 2-4 Business Days</li>
+      <li>Two-Day / Arrives 2-3 Business Days</li>
+      <li>Next-Day / Arrives 1-2 Business Days</li>
+    </div>
+  )
+}
+
+export default ShippingInfo;
