7009e98f48d16ffe9f0d9721252953863f15b416
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-06 20:41:10 -0800

    Wrap each link in span element to add space between links

--- client/components/ShippingInfo.jsx
+++ client/components/ShippingInfo.jsx
@@ -5,8 +5,9 @@ const ShippingInfo = (props) => {
   return (
     <div>
       Free standard shipping and 30-day free returns, only with NikePlus. 
-      <a href=''>Learn more. </a>
-      <a href=''>Return policy exclusions apply. </a>
+      <span> <a href=''>Learn more.</a></span>
+      <span> <a href=''>Return policy exclusions apply.</a></span>
+      <br />
       <br />
       <li>Standard / Arrives 2-4 Business Days</li>
       <li>Two-Day / Arrives 2-3 Business Days</li>
