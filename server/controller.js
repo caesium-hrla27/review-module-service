580db999260040589a08536e59572cacb60aa1ce
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-09 10:13:11 -0800

    Send three most recent reviews for preview

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,13 @@ const dbHelper = require('../database/dbHelper.js')
 
 module.exports = {
   getPreview: (req, res) => {
-
+    dbHelper.getPreview(req.query.productId)
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
   },
   getFullview: (req, res) => {
 
