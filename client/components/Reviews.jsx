d2cc5c11bf7f30971125fac2715b96e9d54cf78d
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-08 20:56:56 -0800

    Refactor to stateful component and add get request function

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
-const Reviews = (props) => {
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_id: '',
+      previews: []
+    }
 
-  return (
-    <div>
-      Reivews
-    </div>
-  )
+    this.fetchPreviews = this.fetchPreviews.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPreviews();
+  }
+
+  fetchPreviews() {
+    axios
+      .get('/side-bar/review/preview', { params: {
+        product_id: this.state.product_id
+      }})
+  }
+
+  render() {
+    return (
+      <div>
+        Reivews
+      </div>
+    )
+  }
 }
 
 export default Reviews;
