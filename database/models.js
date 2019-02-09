5f88e71f404ab3c43c367dfe88e0c97eb702188d
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-08 20:57:38 -0800

    Create association between two tables

--- database/models.js
+++ database/models.js
@@ -11,7 +11,7 @@ const Product = sequelize.define('product', {
     type: Sequelize.STRING,
     unique: true
   },
-  more_info: Sequelize.STRING,
+  more_info: Sequelize.TEXT,
   product_img: Sequelize.STRING,
   price: Sequelize.STRING,
   sales: Sequelize.BOOLEAN,
@@ -30,55 +30,60 @@ const Review = sequelize.define('review', {
   durability: Sequelize.INTEGER,
   rate: Sequelize.INTEGER,
   username: Sequelize.STRING,
-  location: Sequelize.STRING,
+  location: Sequelize.TEXT,
   upvote: Sequelize.INTEGER,
   downvote: Sequelize.INTEGER,
-  comment: Sequelize.STRING,
-  title: Sequelize.STRING,
-  response: Sequelize.STRING,
+  comment: Sequelize.TEXT,
+  title: Sequelize.TEXT,
+  response: Sequelize.TEXT,
   pre_launch: Sequelize.BOOLEAN,
 });
 
 Product.Reviews = Product.hasMany(Review);
 
 module.exports = {
-  saveDummyData: (req, res) => {
-    return sequelize.sync({ force: true })
-      .then(() => Product.create({
-        id: req.body.id,
-        product_name: req.body.product_name,
-        more_info: req.body.more_info,
-        product_img: req.body.product_img,
-        price: req.body.price,
-        sales: req.body.sales,
-        sales_price: req.body.sales_price,
-        reviews: [{
-          size: req.body.size,
-          comfort: req.body.comfort,
-          durability: req.body.durability,
-          rate: req.body.rate,
-          username: req.body.username,
-          location: req.body.location,
-          upvote: req.body.upvote,
-          downvote: req.body.downvote,
-          comment: req.body.comment,
-          title: req.body.title,
-          response: req.body.response,
-          pre_launch: req.body.pre_launch
-        }]
-      }, {
-        include: [{
-          association: Product.Reviews
-        }]
-      }))
-      .then(() => {
-        console.log('saved');
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  },
-  load: (req, res) => {
-
-  }
+  Product,
+  Review
 }
+
+// module.exports = {
+//   saveDummyData: (req, res) => {
+    // return sequelize.sync({ force: true })
+    //   .then(() => Product.create({
+    //     id: req.body.id,
+    //     product_name: req.body.product_name,
+    //     more_info: req.body.more_info,
+    //     product_img: req.body.product_img,
+    //     price: req.body.price,
+    //     sales: req.body.sales,
+    //     sales_price: req.body.sales_price,
+    //     reviews: [{
+    //       size: req.body.size,
+    //       comfort: req.body.comfort,
+    //       durability: req.body.durability,
+    //       rate: req.body.rate,
+    //       username: req.body.username,
+    //       location: req.body.location,
+    //       upvote: req.body.upvote,
+    //       downvote: req.body.downvote,
+    //       comment: req.body.comment,
+    //       title: req.body.title,
+    //       response: req.body.response,
+    //       pre_launch: req.body.pre_launch
+    //     }]
+    //   }, {
+    //     include: [{
+    //       association: Product.Reviews
+    //     }]
+    //   }))
+//       .then(() => {
+//         console.log('saved');
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       })
+//   },
+//   load: (req, res) => {
+
+//   }
+// }
