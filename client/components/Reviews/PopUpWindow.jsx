d3e1b4594eb07f9933c998d294a030ef8fa96627
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-09 11:10:34 -0800

    Add basic structure of PopUpWindow component

---
+++ client/components/Reviews/PopUpWindow.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PopUpWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  fetchInitialReviews() {
+
+  }
+
+  render() {
+
+  }
+}
