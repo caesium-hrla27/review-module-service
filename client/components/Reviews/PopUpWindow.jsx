3c57f569344159bcbfaceba1628d6d0d217e6325
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-10 00:19:14 -0800

    Fetch initial 10 reviews

--- client/components/Reviews/PopUpWindow.jsx
+++ client/components/Reviews/PopUpWindow.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 class PopUpWindow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-    }
+      initialReviews: [],
+      moreReviews: [],
+    };
   }
 
   fetchInitialReviews() {
-
+    axios
+      .get('/side-bar/review/fullview')
+      .then((data) => {
+        this.setState({
+          initialReviews: data.data,
+        });
+        console.log(data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render() {
-
+    return (
+      <div>
+        Hello!
+      </div>
+    );
   }
 }
+
+export default PopUpWindow;
