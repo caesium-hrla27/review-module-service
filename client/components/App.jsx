b51946cd2e0db4928aa2558c9e87fb3cff3a7838
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-10 00:00:59 -0800

    Fix potential bug and add count display feature
    Fix potential bug that might not allow users to close the menu bars and add count display feature that displays the number of reviews in Reviews menu bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,56 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
-import ShippingInfo from './ShippingInfo.jsx';
-import Reviews from './Reviews.jsx';
-import MoreInfo from './MoreInfo.jsx';
-import ArrowUp from './assets/ArrowUp.jsx';
-import ArrowDown from './assets/ArrowDown.jsx';
+import React from "react";
+import styled from "styled-components";
+import ShippingInfo from "./ShippingInfo.jsx";
+import Reviews from "./Reviews.jsx";
+import MoreInfo from "./MoreInfo.jsx";
+import ArrowUp from "./assets/ArrowUp.jsx";
+import ArrowDown from "./assets/ArrowDown.jsx";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      productId: "M1",
       shippingInfoToggle: false,
       reviewsToggle: false,
-      moreInfoToggle: false
-    }
+      moreInfoToggle: false,
+      count: 0
+    };
 
     this.handleShippingClick = this.handleShippingClick.bind(this);
     this.handleReviewsToggle = this.handleReviewsToggle.bind(this);
     this.handleMoreInfoToggle = this.handleMoreInfoToggle.bind(this);
-    this.resetToggles = this.resetToggles.bind(this);
+    this.fetchCount = this.fetchCount.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCount();
   }
 
   handleShippingClick(e) {
     e.preventDefault();
-    this.resetToggles();
     this.setState({
-      shippingInfoToggle: !this.state.shippingInfoToggle
-    })
-  } 
+      shippingInfoToggle: !this.state.shippingInfoToggle,
+      moreInfoToggle: false,
+      reviewsToggle: false,
+    });
+  }
 
   handleReviewsToggle(e) {
     e.preventDefault();
-    this.resetToggles();
     this.setState({
-      reviewsToggle: !this.state.reviewsToggle
-    })
+      reviewsToggle: !this.state.reviewsToggle,
+      shippingInfoToggle: false,
+      moreInfoToggle: false,
+    });
   }
 
   handleMoreInfoToggle(e) {
     e.preventDefault();
-    this.resetToggles();
     this.setState({
-      moreInfoToggle: !this.state.moreInfoToggle
-    })
-  }
-
-  resetToggles() {
-    this.setState({
+      moreInfoToggle: !this.state.moreInfoToggle,
       shippingInfoToggle: false,
       reviewsToggle: false,
-      moreInfoToggle: false
-    })
+    });
+  }
+
+  fetchCount() {
+    var id = this.state.productId;
+    axios
+      .get(`/side-bar/review/count/${id}`, { params: { productId: id } })
+      .then(c => {
+        console.log(c);
+        this.setState({
+          count: c.data[0]
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   render() {
@@ -59,10 +76,12 @@ class App extends React.Component {
     let moreInfo;
 
     if (this.state.shippingInfoToggle) {
-      shippingInfo = <ShippingInfo 
-        title="Free Shipping & Returns" 
-        toggle={this.state.shippingInfoToggle}
-      />;
+      shippingInfo = (
+        <ShippingInfo
+          title="Free Shipping & Returns"
+          toggle={this.state.shippingInfoToggle}
+        />
+      );
     } else if (this.state.reviewsToggle) {
       reviews = <Reviews />;
     } else if (this.state.moreInfoToggle) {
@@ -70,34 +89,26 @@ class App extends React.Component {
     }
     return (
       <div>
-        <div onClick={this.handleShippingClick}>Free Shipping & Returns
-          {this.state.shippingInfoToggle
-          ? <ArrowUp />
-          : <ArrowDown />
-          }
+        <div onClick={this.handleShippingClick}>
+          Free Shipping & Returns
+          {this.state.shippingInfoToggle ? <ArrowUp /> : <ArrowDown />}
         </div>
         {shippingInfo}
         <br />
-        <div onClick={this.handleReviewsToggle}>Reviews
-          <span>
-          {this.state.reviewsToggle
-            ? <ArrowUp />
-            : <ArrowDown />
-          }
-          </span>
+        <div onClick={this.handleReviewsToggle}>
+          Reviews ({this.state.count})
+          <span>{this.state.reviewsToggle ? <ArrowUp /> : <ArrowDown />}</span>
         </div>
         {reviews}
         <br />
-        <div onClick={this.handleMoreInfoToggle}>More Info
-          {this.state.moreInfoToggle
-          ? <ArrowUp />
-          : <ArrowDown />
-          }
+        <div onClick={this.handleMoreInfoToggle}>
+          More Info
+          {this.state.moreInfoToggle ? <ArrowUp /> : <ArrowDown />}
         </div>
         {moreInfo}
-        <br/>
+        <br />
       </div>
-    )
+    );
   }
 }
 
