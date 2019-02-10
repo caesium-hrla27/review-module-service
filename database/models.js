9eb26430ef3ccca67efaada9cdbe3f4126ac8739
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-10 00:23:46 -0800

    Delete commented codes

--- database/models.js
+++ database/models.js
@@ -5,17 +5,17 @@ const Product = sequelize.define('product', {
   id: {
     type: Sequelize.STRING,
     unique: true,
-    primaryKey: true
+    primaryKey: true,
   },
   product_name: {
     type: Sequelize.STRING,
-    unique: true
+    unique: true,
   },
   more_info: Sequelize.TEXT,
   product_img: Sequelize.STRING,
   price: Sequelize.STRING,
   sales: Sequelize.BOOLEAN,
-  sales_price: Sequelize.STRING
+  sales_price: Sequelize.STRING,
 });
 
 const Review = sequelize.define('review', {
@@ -23,7 +23,7 @@ const Review = sequelize.define('review', {
     type: Sequelize.INTEGER,
     unique: true,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
   },
   size: Sequelize.INTEGER,
   comfort: Sequelize.INTEGER,
@@ -43,47 +43,5 @@ Product.Reviews = Product.hasMany(Review);
 
 module.exports = {
   Product,
-  Review
-}
-
-// module.exports = {
-//   saveDummyData: (req, res) => {
-    // return sequelize.sync({ force: true })
-    //   .then(() => Product.create({
-    //     id: req.body.id,
-    //     product_name: req.body.product_name,
-    //     more_info: req.body.more_info,
-    //     product_img: req.body.product_img,
-    //     price: req.body.price,
-    //     sales: req.body.sales,
-    //     sales_price: req.body.sales_price,
-    //     reviews: [{
-    //       size: req.body.size,
-    //       comfort: req.body.comfort,
-    //       durability: req.body.durability,
-    //       rate: req.body.rate,
-    //       username: req.body.username,
-    //       location: req.body.location,
-    //       upvote: req.body.upvote,
-    //       downvote: req.body.downvote,
-    //       comment: req.body.comment,
-    //       title: req.body.title,
-    //       response: req.body.response,
-    //       pre_launch: req.body.pre_launch
-    //     }]
-    //   }, {
-    //     include: [{
-    //       association: Product.Reviews
-    //     }]
-    //   }))
-//       .then(() => {
-//         console.log('saved');
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       })
-//   },
-//   load: (req, res) => {
-
-//   }
-// }
+  Review,
+};
