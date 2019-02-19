a3f846765da11495c8045ba57640d281d474350a
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-19 01:45:57 -0800

    Style previews and style more reviews button

--- client/components/Reviews/Previews.jsx
+++ client/components/Reviews/Previews.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import PreviewEntry from './PreviewEntry.jsx';
 import PopUpWindow from './PopUpWindow.jsx';
+import Popup from 'react-skylight';
 
+const PreviewWrapper = styled.div`
+  align-items: flex-start;
+  text-align: left;
+  padding-left: 5px;
+`
+
+const MoreReviews = styled.button`
+  outline: none;
+  border: none;
+  cursor: pointer;
+  padding-bottom: 4px;
+  border-bottom: 1px solid black;
+  border-radius: unset;
+  font-size: 14px;
+  text-align: center;
+`
 class Previews extends React.Component {
   constructor(props) {
     super(props);
@@ -27,22 +45,29 @@ class Previews extends React.Component {
     if (moreReviewsToggle) {
       moreReviews = <PopUpWindow exit={this.handleMoreReviewsToggle} />;
     }
+    const myPopUp = {
+      width: '100%',
+      height: '100%',
+      marginTop: '-300px',
+      marginLeft: '-35%',
+    }
     return (
-      <div className="previews">
+      <PreviewWrapper>
         {previews.map((preview, index) => (
           <div className="preview">
             <PreviewEntry preview={preview} key={index} />
           </div>
         ))}
         <br />
-        <button
-          className="moreReviews"
-          onClick={this.handleMoreReviewsToggle}
+        <MoreReviews onClick={() => this.customDialog.show()}>More Reviews</MoreReviews>
+        <Popup dialogStyles={myPopUp}
+          hideOnOverlayClicked 
+          ref={ref => this.customDialog = ref} 
+          title="Pop Up Window"
         >
-        More Reviews
-        </button>
-        {moreReviews}
-      </div>
+          <PopUpWindow exit={this.handleMoreReviewsToggle} />
+        </Popup>
+      </PreviewWrapper>
     );
   }
 }
