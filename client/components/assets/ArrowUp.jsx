5f1f6715249e87e7ccb8fa71ad2b98f056cc35a3
KweonMo Koo <mikekoo2010@gmail.com>
2019-02-19 01:46:42 -0800

    Resize arrows

--- client/components/assets/ArrowUp.jsx
+++ client/components/assets/ArrowUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArrowUp = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="8">
       <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
       <path d="M0 0h24v24H0z" fill="none"/>
     </svg>
